3e976ce4c95dabe17246e91b4dc19f2cc12c0c26
Sam Jeston <sam.jeston@iohk.io>
2019-03-19 16:40:24 +1000

    README update

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 A service that orchestrates the creation of Docker containers from x86 smart contract bytecode, and the proxying of HTTP requests to these containers.
 
+## Usage
+
+This service exposes an API that provides access to the following behaviour:
+
+### loadContainer
+Create a Docker container running a smart contract executable. The executable must be a base64-encoded x86 binary that exposes the Plutus smart contract HTTP interface.
+
+### unloadContainer
+Kill a running Docker container, by contract address.
+
+### execute
+Call a specified smart contract container with method and arguments. If the container does not exist, an error is thrown that prompts the caller to first load the container.
+
 ## Development
 
 ### Testing
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import { findContainerPort, loadContainer, unloadContainer } from './docker-api'
 import * as bodyParser from 'body-parser'
 import axios from 'axios'
 
-export function configureApi ({ port, lowerPortBound, upperPortBound }: { port: number, lowerPortBound: number, upperPortBound: number }) {
+export function configureApi({ port, lowerPortBound, upperPortBound }: { port: number, lowerPortBound: number, upperPortBound: number }) {
   const app = express()
   app.use(bodyParser.json({ limit: '50mb' }))
 
@@ -54,17 +54,17 @@ export function configureApi ({ port, lowerPortBound, upperPortBound }: { port: 
   return app.listen(port)
 }
 
-export function bootApi () {
-  const { API_PORT, LOWER_PORT_BOUND, UPPER_PORT_BOUND } = process.env
+export function bootApi() {
+  const { API_PORT, CONTAINER_LOWER_PORT_BOUND, CONTAINER_UPPER_PORT_BOUND } = process.env
 
-  if (!API_PORT || !LOWER_PORT_BOUND || !UPPER_PORT_BOUND) {
+  if (!API_PORT || !CONTAINER_LOWER_PORT_BOUND || !CONTAINER_UPPER_PORT_BOUND) {
     throw new Error('Missing environment config')
   }
 
   configureApi({
     port: Number(API_PORT),
-    lowerPortBound: Number(LOWER_PORT_BOUND),
-    upperPortBound: Number(UPPER_PORT_BOUND)
+    lowerPortBound: Number(CONTAINER_LOWER_PORT_BOUND),
+    upperPortBound: Number(CONTAINER_UPPER_PORT_BOUND)
   })
 
   console.log(`Smart Contract Docker Engine listening on Port ${API_PORT}`)
